25201c6747317ba29d2c2558ea11e0aed686dbd6
Margo Matskevych <m_matskevych@MacBook-Pro-Margo.local>
2025-04-02 13:18:20 +0300

    complete tasks

--- task2.js
+++ task2.js
@@ -5,3 +5,34 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.getElementById("userNameInput");
+const getUserButton = document.getElementById("getUserButton");
+const userCity = document.getElementById("userCity");
+
+getUserButton.addEventListener("click", () => {
+  const userName = userNameInput.value.trim();
+  if (userName === "") {
+    return;
+  }
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      let userFound = false;
+      for (const user of users) {
+        if (user.name === userName) {
+          userCity.textContent = user.address.city;
+          userFound = true;
+          break;
+        }
+      }
+
+      if (!userFound) {
+        userCity.textContent = "User not found";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
